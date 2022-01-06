8ca53fb0d63f4f087b1f53549b5050d8fbdc7f55
"badalharit" <"badalharitmrt@gmail.com">
2022-01-06 21:44:53 +0530

    `Enum` | Lottery Algo

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -6,25 +6,27 @@ contract the_lottery{
     struct crypto_user{
         string name; 
         uint account_balance;
-        string status;
+        bool can_redeem;
         }
     enum user{allowed,not_allowed,wait}
     crypto_user public badal;
     
     constructor(){
         badal.name = "Badal Harit";
+        badal.can_redeem = true;
     }
     
 
-    user public allocate_lottery = user.allowed;
+    user allocate_lottery = user.allowed;
     uint public lottery = 1000;
 
     function redeem() public{
-        if(allocate_lottery == user.allowed){
+        if(allocate_lottery == user.allowed && badal.can_redeem == true){
             uint lottery_amt;
             lottery_amt = (lottery/4);
             lottery = lottery - lottery_amt;
-            badal.account_balance = lottery_amt;
+            badal.account_balance += lottery_amt;
+            badal.can_redeem = false;
         }
     }
     
